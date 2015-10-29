9cb5cd2489f4e45a85292c68f893db360378089d
Alex Sims <alexander.sims92@gmail.com>
2015-10-29 18:05:26 +0000

    Todays push

---
+++ app/views/projects/kerr.php
@@ -0,0 +1,46 @@
+<div class="main-wrapper container-fluid">
+    <!-- Banner -->
+    <div class="hero blue-green-bg text-center">
+        <div class="container section">
+            <h1 class="white">LET'S CHAT</h1>
+            <p class="white">Call us on
+                <strong>0747 233 0050
+                    <strong> or email us at
+                        <strong>info@kodadevelopment.com</strong>
+            </p>
+            <p class="white">Get in touch with our friendly team for a quote for your idea.</p>
+
+            <div class="col-lg-12 offset-top">
+                <div class="row social large">
+                    <a target="_blank" href="http://instagram.com/kodadevelopment"><i class="fa fa-instagram"></i></a>
+                    <a target="_blank" href="http://facebook.com/kodadevelopment"><i class="fa fa-facebook"></i></a>
+                    <a target="_blank" href="http://twitter.com/kodadevelopment"><i class="fa fa-twitter"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Section A -->
+    <div class="section text-center">
+        <div class="container section">
+            <div class="col-lg-12 col-md-12 col-sm-12 text-center">
+                <div style="margin-top:70px;">
+                    <h1>OR EMAIL US DIRECTLY</h1>
+                    <p>Please disclose someinformation about your project such as your budget and deadline and we'll get back to you within 48 hours.</p>
+
+                    <form method="POST" action="#" class="contact-form">
+                        <label>*</label>
+                        <input type="text" placeholder="Name..." name="name" required>
+                        <br/>
+                        <label>*</label>
+                        <input type="text" placeholder="Email..." name="email" required>
+                        <br/>
+                        <label class="pullUp">*</label>
+                        <textarea placeholder="Your message..." required></textarea>
+                        <br/>
+                        <button class="send">SEND</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
